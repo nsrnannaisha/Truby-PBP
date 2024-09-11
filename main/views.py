52d4c6bce3fea8fc91e799fefd56c279846f9cb1
nsrnannaisha <nisrinaannaisha@gmail.com>
2024-09-11 10:57:22 +0700

    Improve tampilan

--- main/views.py
+++ main/views.py
@@ -3,8 +3,13 @@ from django.shortcuts import render
 def show_main(request):
     context = {
         'application' : 'Trubuy',
-        'name': 'Nisrina Annaisha Sarnadi',
-        'class': 'PBP F'
+        'self_name': 'Nisrina Annaisha Sarnadi',
+        'class': 'PBP F',
+        'name': 'BRUNBÅGE Desk Lamp',
+        'price': 'Rp349.000',
+        'description': 'LED desk lamp with a storage that can be dimmed' ,
+        'rating': '5/5',
+        'quantity': '17'
     }
 
     return render(request, "main.html", context)
