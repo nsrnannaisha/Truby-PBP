f605b7e59286e2085027c60e14cccad67106c645
nsrnannaisha <nisrinaannaisha@gmail.com>
2024-09-19 16:58:38 +0700

    Initial commit tugas 4: Implementasi autentikasi, session, dan cookies

--- main/views.py
+++ main/views.py
@@ -1,15 +1,23 @@
+import datetime
 from django.shortcuts import render, redirect
 from main.forms import ProductEntryForm
 from main.models import ProductEntry
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='/login')
 
 def show_main(request):
-    product_entries = ProductEntry.objects.all()
+    product_entries = ProductEntry.objects.filter(user=request.user)
 
     context = {
         'application' : 'Trubuy',
-        'self_name': 'Nisrina Annaisha Sarnadi',
+        'self_name': request.user.username,
         'class': 'PBP F',
         'name': 'BRUNBÅGE Desk Lamp',
         'price': 'Rp349.000',
@@ -17,6 +25,7 @@ def show_main(request):
         'rating': '5/5',
         'quantity': '17',
         'product_entries': product_entries,
+        'last_login': request.COOKIES['last_login'],
 
     }
 
@@ -26,7 +35,9 @@ def add_product(request):
     form = ProductEntryForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        ProductEntry = form.save(commit=False)
+        ProductEntry.user = request.user
+        ProductEntry.save()
         return redirect('main:show_main')
 
     context = {'form': form}
@@ -47,3 +58,38 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = ProductEntry.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+                user = form.get_user()
+                login(request, user)
+                response = HttpResponseRedirect(reverse("main:show_main"))
+                response.set_cookie('last_login', str(datetime.datetime.now()))
+                
+                return response
+
+    else:
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
