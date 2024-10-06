aeb0fbce406a3ef0c9f1458458501d264b652851
nsrnannaisha <nisrinaannaisha@gmail.com>
2024-10-06 11:38:52 +0700

    solving error

--- main/models.py
+++ main/models.py
@@ -8,9 +8,5 @@ class ProductEntry(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
-    rating = models.DecimalField(max_digits=3, decimal_places=1) 
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
     quantity = models.IntegerField()
-    
-    @property
-    def is_out_of_stock(self):
-        return self.quantity == 0
